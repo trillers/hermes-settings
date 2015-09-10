0b6a9a533fda8bee82fb7ccbe3391a4525797500
leekangtaqi <leekangtaqi@sina.com>
2015-09-10 19:54:14 +0800

    add auth

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ var settings = module.exports = {
     mode:'single',
     host: '127.0.0.1',
     port: 6379,
-    auth:'',
+    auth:'trillers',
     sentinel: {
       hosts:[{host: '127.0.0.1', port: 26379}],
       masterName:'mymaster'
